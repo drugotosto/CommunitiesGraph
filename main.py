0c99eb311f1e64ddd54dba3192426d1bef9d00e6
Maurizio <drugotosto@gmail.com>
2016-12-20 11:15:04 +0100

    Il  tentativo di calcolo della proiezione sugli users dal grafo bipartito (User-Tag) tramite utilizzo di Igraph/NetworkX non è andata  a buon fine! Occorre ripiegare sull'utilizzo di Spark!

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ __author__ = 'maury'
 
 import json
 import time
-import networkx
+import networkx as nx
 from igraph import *
 from os.path import isfile
 from networkx.algorithms import bipartite
 
 # Permette di definire la tipologia di grafo bipartito (user-cat) (user-bus) ...
-tipoGrafo="friends"
+tipoGrafo="cat"
+single=False
 categoria="Nightlife"
 communitiesTypes=["fastgreedy", "walktrap", "label_propagation", "infomap", "multilevel"]
 communityType="all"
 pathInput="/home/maury/Desktop/ClusteringMethods/InputData/"
-fileNameInput="user_"+tipoGrafo+"_("+categoria+").json"
 pathOutput="/home/maury/Desktop/ClusteringMethods/OutputData/"
 fileNameGraph="user_"+tipoGrafo+"_("+categoria+")_graph"
 fileNameGraphML="user_"+tipoGrafo+"("+categoria+")_graphML"
@@ -25,31 +25,30 @@ def createGraph(inputData,tipoGrafo):
     g=Graph()
     if tipoGrafo!="friends":
         # Creo i vertici (business/tags) del grafo bipartito
-        values={value for lista in inputData.values() for value in lista}
+        values={elem for lista in inputData.values() for elem,_ in lista}
         for value in values:
-            g.add_vertex(name=value,gender=tipoGrafo,label=value)
+            g.add_vertex(name=value,gender=tipoGrafo,label=value,bipartite=1)
 
         # Recupero i vertici (users) del grafo bipartito
         users={user for user in inputData.keys()}
         for user in users:
-            g.add_vertex(name=user,gender="user",label=user)
-
+            g.add_vertex(name=user,gender="user",label=user,bipartite=0)
     else:
         # Recupero i vertici (users) del grafo delle amicizie
         users={user for user in inputData.keys()}.union({friend for listFriends in inputData.values() for friend,weight in listFriends})
         for user in users:
             g.add_vertex(name=user,gender="user",label=user)
 
-        def createPairs(user,listFriends):
-            return [(user,friend) for friend,_ in listFriends]
+    def createPairs(user,listItems):
+        return [(user,item[0]) for item in listItems]
 
-        # Creo gli archi (di amicizia) del grafo
-        listaList=[createPairs(user,listFriends) for user,listFriends in inputData.items()]
-        archi=[(user,friend) for lista in listaList for user,friend in lista]
-        g.add_edges(archi)
-        # Aggiungo i relativi pesi agli archi
-        weights=[weight for user,listFriends in inputData.items() for friend,weight in listFriends]
-        g.es["weight"]=weights
+    # Creo gli archi (amicizie/tag) del grafo
+    listaList=[createPairs(user,listItems) for user,listItems in inputData.items()]
+    archi=[(user,elem) for lista in listaList for user,elem in lista]
+    g.add_edges(archi)
+    # Aggiungo i relativi pesi agli archi
+    weights=[weight for user,listItems in inputData.items() for _,weight in listItems]
+    g.es["weight"]=weights
     return g
 
 def saveGraphs(g):
@@ -57,7 +56,13 @@ def saveGraphs(g):
     g.write_graphml(f=open(pathOutput+fileNameGraphML+".graphml","wb"))
 
 def creazioneGrafo(tipoGrafo):
+    fileNameInput="user_"+tipoGrafo+"_("+categoria+").json"
     # Recupero i dati dal file Json
+    if tipoGrafo=="cat":
+        if single:
+            fileNameInput="user_"+tipoGrafo+"_("+categoria+")_singleWeight.json"
+        else:
+            fileNameInput="user_"+tipoGrafo+"_("+categoria+")_2moreWeight.json"
     diz=recuperaDati(fileName=pathInput+fileNameInput)
     # Creo il grafo
     g=createGraph(inputData=diz,tipoGrafo=tipoGrafo)
@@ -66,62 +71,89 @@ def creazioneGrafo(tipoGrafo):
     saveGraphs(g)
     return g
 
-def creazioneProiezione():
-    g=Graph.Read_Pickle(fname=open(pathOutput+fileNameGraph))
-    g1=g.bipartite_projection(which=1)
-    # bipar=g.bipartite_projection(types="proiezione",which=0)
-    # summary("\n{}".format(bipar))
+def creazioneProiezione(g):
+    """
+    Dal grafo bipartito (User-Tag) vado a generare la proiezione sugli users
+    :param g:
+    :type g: Graph
+    :return:
+    """
+    def my_weight(G, u, v, weight='weight'):
+        w = 0
+        for nbr in set(G[u]) & set(G[v]):
+            w += G.edge[u][nbr].get(weight, 1) + G.edge[v][nbr].get(weight, 1)
+        return w
 
-def creazioneCommunities(g,communityType):
-    # calculate dendrogram
-    dendrogram=None
-    clusters=None
-    if communityType=="all":
-        types=communitiesTypes
+    print("\nVado a creare prima il grafo bipartito e poi la proiezione sugli utenti!")
+    # Passo attraverso Networkx
+    B=nx.read_graphml(path=pathOutput+fileNameGraphML+".graphml")
+    if nx.is_connected(B):
+        top_nodes = set((n,d["gender"]) for n,d in B.nodes(data=True) if d['bipartite']==1)
+        bottom_nodes = set(B) - top_nodes
+        print("\nGrafo bipartito?: {}".format(nx.is_bipartite(B)))
+        print("NODI: {}".format(list(top_nodes)[:10]))
+        G = bipartite.generic_weighted_projected_graph(B,bottom_nodes,weight_function=my_weight)
+        print("\nArchi: {}".format(G.edges(data=True)[:10]))
+
+def creazioneCommunities(g):
+    if tipoGrafo=="friends":
+        print("\nCreo le communities in base alla friendships!")
+        # calculate dendrogram
+        dendrogram=None
+        clusters=None
+        if communityType=="all":
+            types=communitiesTypes
+        else:
+            types=[communityType]
+
+        for type in types:
+            if type=="fastgreedy":
+                dendrogram=g.community_fastgreedy(weights="weight")
+            elif type=="walktrap":
+                dendrogram=g.community_walktrap(weights="weight")
+            elif type=="label_propagation":
+                clusters=g.community_label_propagation(weights="weight")
+            elif type=="multilevel":
+                clusters=g.community_multilevel(weights="weight",return_levels=False)
+            elif type=="infomap":
+                clusters=g.community_infomap(edge_weights="weight")
+
+            # convert it into a flat clustering (VertexClustering)
+            if type!="label_propagation" and type!="multilevel" and type!="infomap":
+                clusters = dendrogram.as_clustering()
+            # get the membership vector
+            membership = clusters.membership
+            json.dump([(name,membership) for name, membership in zip(g.vs["name"], membership)],open(pathOutput+"communities_friends_("+categoria+")_"+type+".json","w"))
+            print("Clustering Summary for '{}' : {}".format(type,clusters.summary()))
     else:
-        types=[communityType]
-
-    for type in types:
-        if type=="fastgreedy":
-            dendrogram=g.community_fastgreedy(weights="weight")
-        elif type=="walktrap":
-            dendrogram=g.community_walktrap(weights="weight")
-        elif type=="label_propagation":
-            clusters=g.community_label_propagation(weights="weight")
-        elif type=="multilevel":
-            clusters=g.community_multilevel(weights="weight",return_levels=False)
-        elif type=="infomap":
-            clusters=g.community_infomap(edge_weights="weight")
-
-        # convert it into a flat clustering (VertexClustering)
-        if type!="label_propagation" and type!="multilevel" and type!="infomap":
-            clusters = dendrogram.as_clustering()
-        # get the membership vector
-        membership = clusters.membership
-        json.dump([(name,membership) for name, membership in zip(g.vs["name"], membership)],open(pathOutput+"communities_friends_("+categoria+")_"+type+".json","w"))
-        print("Clustering Summary for '{}' : {}".format(type,clusters.summary()))
+        print("\nCreo le overlapping communities in base alle relazioni (User-Tag) esistenti!")
+        pass
 
 if __name__ == '__main__':
-    startTime=time.time()
-    g=creazioneGrafo(tipoGrafo)
-    print("\nIl grafo e stato creato! Tempo: {} min.".format((time.time()-startTime)/60))
-    print("Summary:\n{}".format(summary(g)))
-
-    if tipoGrafo=="friends":
-        # Sto andando a trovare le communities delle amicizie
+    if not isfile(pathOutput+fileNameGraph):
+        print("\nVado a creare il grafo!")
         startTime=time.time()
-        creazioneCommunities(g,communityType)
-        print("\nFinito di calcolare le communities in TEMPO: {}".format((time.time()-startTime)/60))
-"""
+        g=creazioneGrafo(tipoGrafo)
+        print("\nIl grafo e stato creato! Tempo: {} min.".format((time.time()-startTime)/60))
+        print("Summary:\n{}".format(summary(g)))
+
+        if tipoGrafo=="friends":
+            # Sto andando a trovare le communities delle amicizie
+            startTime=time.time()
+            creazioneCommunities(g)
+            print("\nFinito di calcolare le communities in TEMPO: {}".format((time.time()-startTime)/60))
+
+        else:
+            # Dal grafo bipartito (Users-Tags)/(Users-Business) vado a creare la proiezione sugli users e da qui trovo le communities (overlapping)
+            startTime=time.time()
+            print("\nFinito di calcolare la proiezione in TEMPO: {}".format((time.time()-startTime)/60))
     else:
-        # Dal grafo bipartito (Users-Tags)/(Users-Business) vado a creare la proiezione sugli users e da qui trovo le communities (overlapping)
-        creazioneProiezione()
-        startTime=time.time()
-        creazioneCommunities(g)
-        print("\nFinito di calcolare le commuities in TEMPO: {}".format((time.time()-startTime)/60))
+        print("\nVado a prelevare il grafo!")
+        g=Graph.Read_Pickle(fname=open(pathOutput+fileNameGraph))
+        if tipoGrafo=="friends":
+            startTime=time.time()
+            creazioneCommunities(g)
+            print("\nFinito di calcolare le communities in TEMPO: {}".format((time.time()-startTime)/60))
+        else:
+            creazioneProiezione(g)
 
-    # Passo attraverso Networkx
-    a=g.get_edgelist()
-    b=networkx.Graph(a)
-    bottom_nodes, top_nodes = bipartite.sets(b)
-"""
